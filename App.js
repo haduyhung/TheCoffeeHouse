50578ed7d6ced85245b6db5554b33610a37c5e65
HungMaxDZ <haduyhungdz123@gmail.com>
2021-08-19 22:12:44 +0700

    add icons

--- App.js
+++ App.js
@@ -1,10 +1,55 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Touchable } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
-    <View>
-      <Text>Hello hehehe</Text>
+    <View style={styles.container}>
+      <View style={styles.up}>
+        <View>
+          <Text>Xin chào Hưng Max DZ!</Text>
+        </View>
+        <View>
+          <TouchableOpacity style={styles.taoVoucher}>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.middle}>
+        <Ionicons name="home-outline" size={30} color="gray" />
+      </View>
+      <View style={styles.down}>
+
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: 'white',
+  },
+  up: {
+    flex: 2 / 3,
+    backgroundColor: 'red',
+  },
+  taoVoucher: {
+    borderWidth: 1,
+
+  },
+  middle: {
+    flex: 8,
+    backgroundColor: 'green',
+  },
+  down: {
+    flex: 3 / 4,
+    backgroundColor: 'blue',
+  },
+
+})
